52aabc4480ba932e22a76f495a015123eb217d0c
zeerak-aziz <zeerakazizwork@gmail.com>
2025-03-24 13:39:45 +0000

    updated to more verbose unit test names

--- test_clean_email.py
+++ test_clean_email.py
@@ -3,27 +3,27 @@ from clean_email import clean_email
 
 
 # PyTest unit tests
-def test_clean_email_normal():
+def test_clean_email_masks_standard_email_format():
     text = "hello, you can contact me at zeerak.aziz@example.com"
     assert clean_email(text) == "hello, you can contact me at ****"
 
 
-def test_clean_email_multiple():
+def test_clean_email_masks_multiple_emails_in_text():
     text = "emails: user1@domain.com, user2@example.org"
     assert clean_email(text) == "emails: ****, ****"
 
 
-def test_clean_email_no_email():
+def test_clean_email_retains_text_with_no_emails():
     text = "no email here!"
     assert clean_email(text) == "no email here!"
 
 
-def test_clean_email_edge():
+def test_clean_email_invalid_email_format():
     text = "email: admin@something"
     assert clean_email(text) == "email: ****"
 
 
-def test_clean_email_mixed():
+def test_clean_email_retains_website_name_and_masks_standard_email():
     text = "contact at john.doe@example.com for info, or visit example.com"
     assert clean_email(text) == "contact at **** for info, or visit example.com"
 
